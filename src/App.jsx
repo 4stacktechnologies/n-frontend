39051818b1e03457e4c21a68e20fcac667e7fad6
Rohan123code <rohanfasate2007@gmail.com>
2025-12-29 21:56:11 +0530

    feat: restructure App component layout; add Footer component and implement slideshow with categories and top deals in Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { AuthProvider } from "./auth/AuthContext";
 import AppRoutes from "./routes/AppRoutes";
+import Footer from "./components/Footer"; // Import the Footer component
 
 export default function App() {
   return (
     <AuthProvider>
-      <AppRoutes />
-     </AuthProvider>
+      <div className="flex flex-col min-h-screen">
+        {/* Main content grows */}
+        <div className="flex-grow">
+          <AppRoutes />
+        </div>
+
+        {/* Footer always at the bottom */}
+        <Footer />
+      </div>
+    </AuthProvider>
   );
 }
